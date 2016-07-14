9e7ab0f28acaed294dd41a0442060e5a2dce8ab0
Quoc <nguyenminhqoc@gmail.com>
2016-07-14 12:01:29 +0700

    bug: not show ui

--- ElfParser/src/ElfParserMainForm.java
+++ ElfParser/src/ElfParserMainForm.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JFileChooser;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -60,6 +63,11 @@ public class ElfParserMainForm extends javax.swing.JFrame {
 
     btnOpen.setText("Open");
     btnOpen.setName(""); // NOI18N
+    btnOpen.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        btnOpenActionPerformed(evt);
+      }
+    });
 
     btnReset.setText("Reset");
 
@@ -215,6 +223,12 @@ public class ElfParserMainForm extends javax.swing.JFrame {
 
     pack();
   }// </editor-fold>//GEN-END:initComponents
+
+  private void btnOpenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOpenActionPerformed
+    // TODO add your handling code here:
+    JFileChooser openFile = new JFileChooser();
+    openFile.showOpenDialog(null);
+  }//GEN-LAST:event_btnOpenActionPerformed
 
   /**
    * @param args the command line arguments
